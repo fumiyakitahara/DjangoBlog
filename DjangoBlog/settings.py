5ff978b720a88d1299e642a44bba969f32a15953
fumiyasaitou <saitofumiyaxx@gmail.com>
2022-06-21 00:46:55 +0900

    settingまとめ

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -14,18 +14,31 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+"""
+__file__は現在のモジュールの絶対パスを取得するもの
+resolve()は相対パスを絶対パスに変換するメソッド
+"""
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-k)w1_3wzlad3h6oadao0*-p^be@))qpzq3(x+p=b&@86g2^xi)'
+"""
+利用するユーザーのパスワードに加えられる文字列が登録されている
+"""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+"""
+開発環境の時True
+本番環境の時False
+"""
 
 ALLOWED_HOSTS = []
+"""
+ホストサーバーを登録する
+"""
 
 
 # Application definition
@@ -53,12 +66,12 @@ ROOT_URLCONF = 'DjangoBlog.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',  #テンプレートエンジンとしてDjangoTemplatesモジュールが指定されている
+        'DIRS': [], #検索対象のフォルダー
+        'APP_DIRS': True, #アプリのテンプレートも検索するかどうか
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
+                'django.template.context_processors.debug', #共通の変数名を指定している。この場合だとdebug,request,auth,messages.
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -67,8 +80,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoBlog.wsgi.application'
-
+WSGI_APPLICATION = 'DjangoBlog.wsgi.application' 
+#WSGI ウェブサーバーとウェブアプリを接続するための仕組み
+#この環境変数はWSGIを実行するための関数を登録するためのもの
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -83,19 +97,19 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+#パスワードの妥当性をチェックするための仕組みを登録している
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',#ユーザー名と似たパスワード
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',#短すぎるパスワード
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',#よくあるパスワード
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',#数値のみのパスワード
     },
 ]
 
@@ -103,21 +117,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'#ジャンゴの中で使用する言語
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'#UTCは日本標準時-9h
 
-USE_I18N = True
+USE_I18N = True#多言語化機能
 
-USE_L10N = True
+USE_L10N = True#日付フォーマットなどを地域ごとに適用する機能
 
-USE_TZ = True
+USE_TZ = True#タイムゾーンを変換する機能
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'#静的ファイルを収めたフォルダーのURL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
