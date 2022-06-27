9c52b68a57da88648223ae536410f5a933e7e3ff
fumiyasaitou <saitofumiyaxx@gmail.com>
2022-06-27 23:03:51 +0900

    設定捕捉

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -146,3 +146,11 @@ MESSAGE_TAGS = {
 
 # フォームの送信データをターミナルに出力
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+"""
+DEFAULT_FROM_EMAIL = 'xxxxxx@gmail.com'  # メールの送信元のアドレスを入力
+EMAIL_HOST = 'smtp.gmail.com'            # GmailのSMPTサーバー
+EMAIL_PORT = 587                         # SMPTサーバーのポート番号
+EMAIL_HOST_USER = 'xxxxxx@gmail.com'     # Gmailのアドレスを入力
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxxxxx' # Gmailのアプリ用パスワードを入力
+EMAIL_USE_TLS = True # SMTP サーバと通信する際に TLS (セキュア) 接続を使う
+"""
